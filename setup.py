14fcd8222ca500f67dd4a344b6f295d452b6e781
userbotxxx <88811651+userbotxxx@users.noreply.github.com>
2023-06-21 16:39:38 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,14 +4,14 @@ with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="CHECKER-LIBRARY",
+    name="berlinchklib",
     version="0.0.1",
     author="BERLIN",
     author_email="author@example.com",
     description="A small example package",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/userbotxxx/CHECKER-LIBRARY",
+    url="https://github.com/userbotxxx/berlinchklib",
     project_urls={
         "Bug Tracker": "https://github.com/pypa/sampleproject/issues",
     },
